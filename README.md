8346ce2a8b5e537a60615d7cae7fde1b574f5279
Angelo Francescoly Dias Goncalves <angelo.goncalves@spread.com.br>
2025-07-10 21:36:08 -0300

    Cypress com cucumber

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # autFrontBackCypress
 Repositório com todo material do curso de Automação Front-end e Back-end utilizando Cypress + Cucumber.
+
+Repositório para FRONT-END
+Cada pasta na raiz do e2e representa um produto.
---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    setupNodeEvents(on, config) {
+      on("file:preprocessor", cucumber());
+
+      const version = config.env.version || 'dev'
+      config.env = require(`./cypress/config/${version}.json`);
+      config.baseUrl = config.env.baseUrl;
+      return config;
+    },
+  },
+});
